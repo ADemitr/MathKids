6c9b97961a29c7166febfa00e7b50870a8bc3ead
Dmitry Antonov <dmitry.vl.antonov@gmail.com>
2021-07-15 19:57:54 +0300

    Add commands for buttons

--- MathKidsGame/WpfUI/MainWindowVM.cs
+++ MathKidsGame/WpfUI/MainWindowVM.cs
@@ -1,16 +1,43 @@
-﻿using System.ComponentModel;
+﻿using MathKidsCore;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace WpfUI
 {
-    public class MainWindowVM : INotifyPropertyChanged
+    public class MainWindowVM : BindableBase
     {
-        private string _record = "Hello World";
+        private GameController _gameController;
+        public string Description { get; private set; }
+        public DelegateCommand SayYes { get; }
+        public DelegateCommand SayNo { get; }
+
+        public MainWindowVM()
+        {
+            _gameController = new GameController();
+            NextRound();
+            SayYes = new DelegateCommand(() =>
+                {
+                    Description = "Yes";
+                    RaisePropertyChanged(nameof(Description));
+                });
+            SayNo = new DelegateCommand(() =>
+            {
+                Description = "No";
+                RaisePropertyChanged(nameof(Description));
+            });
+        }
+
+        private void NextRound()
+        {
+            Description = _gameController.GenerateMathTaskAndGetDescription();
+            RaisePropertyChanged(nameof(Description));
+        }
+
+        private string _record = "Рекорд : 0";
         public string Record
         {
             get { return _record; }
             set { _record =  value; }
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
