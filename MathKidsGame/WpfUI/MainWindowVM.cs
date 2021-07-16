6634402b3db29a0fd793459b8982655c9e4a0640
Dmitry Antonov <dmitry.vl.antonov@gmail.com>
2021-07-16 06:47:45 +0300

    Add progress bar and connect game logic

--- MathKidsGame/WpfUI/MainWindowVM.cs
+++ MathKidsGame/WpfUI/MainWindowVM.cs
@@ -1,39 +1,59 @@
 ﻿using MathKidsCore;
 using Prism.Commands;
 using Prism.Mvvm;
+using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace WpfUI
 {
     public class MainWindowVM : BindableBase
     {
         private GameController _gameController;
-        public string Record => $"Рекорд : { _gameController.MaxInARow }";
+        public string BestScore => $"Рекорд : { _gameController.MaxInARow }";
+        public string CurrentScore => $"Счет : { _gameController.CurrentInARow }";
         public string Description { get; private set; }
+        public int TimeProgress { get; private set; }
         public DelegateCommand SayYes { get; }
         public DelegateCommand SayNo { get; }
+        public bool ButtonsEnabled { get; private set; }
 
         public MainWindowVM()
         {
             _gameController = new GameController();
             NextRound();
-            SayYes = new DelegateCommand(() =>
-                {
-                    Description = "Yes";
-                    RaisePropertyChanged(nameof(Description));
-                });
-            SayNo = new DelegateCommand(() =>
+            SayYes = new DelegateCommand(() => CheckResult(_gameController.CheckAnswerAndCancelCountDown(true)));
+            SayNo = new DelegateCommand(() => CheckResult(_gameController.CheckAnswerAndCancelCountDown(false)));
+            _gameController.OnCountDown += (o, progress) =>
             {
-                Description = "No";
-                RaisePropertyChanged(nameof(Description));
-            });
+                TimeProgress = progress;
+                RaisePropertyChanged(nameof(TimeProgress));
+            };
+            _gameController.OnTimeForMathTaskUp += (o, e) => TimeIsUp();
         }
 
         private void NextRound()
         {
             Description = _gameController.GenerateMathTaskAndGetDescription();
-            RaisePropertyChanged(nameof(Description));
-            RaisePropertyChanged(nameof(Record));
+            ButtonsEnabled = true;
 
+            RaisePropertyChanged(nameof(Description));
+            RaisePropertyChanged(nameof(BestScore));
+            RaisePropertyChanged(nameof(CurrentScore));
+            RaisePropertyChanged(nameof(ButtonsEnabled));
+        }
+
+        private void TimeIsUp()
+        {
+            CheckResult(false);
+        }
+
+        private void CheckResult(bool solvedCorrect)
+        {
+            ButtonsEnabled = false;
+            RaisePropertyChanged(nameof(ButtonsEnabled));
+
+            Task.Delay(250)
+                .ContinueWith(t => NextRound());
         }
     }
 }
