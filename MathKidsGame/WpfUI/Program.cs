b34af2b2f39013a60412f201ac301e8ea54bc69e
Dmitry Antonov <dmitry.vl.antonov@gmail.com>
2021-08-19 14:03:38 +0300

    Delete app.xaml, add Program.cs

---
+++ MathKidsGame/WpfUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfUI.View;
+
+namespace WpfUI
+{
+
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.ShowDialog();
+        }
+    }
+}
